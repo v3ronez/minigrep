fe765eb7f00bc01983807b0de24c959ec4142b87
v3ronez <v3ronez.dev@gmail.com>
2025-06-23 11:28:57 -0300

    get args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    // let args: Vec<String> = env::args().collect(); //another way to type collect
+    let query = &args[1];
+    let path = &args[2];
+    println!("Searching for {query}");
+    println!("In file {path}");
 }
